b9c066cb0459c94737842522bf1a9f25c7a6926e
hamzahamedi223 <hamza@isima.u-monastir.tn>
2024-07-21 13:28:56 +0200

    TEST

--- components/modal/store-modal.tsx
+++ components/modal/store-modal.tsx
@@ -1,19 +1,85 @@
-"use client"
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import { Modal } from "@/components/modal";
-import { useStoreModal } from "@/hooks/use-store-modal"; 
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Store name should be minimum 3 characteres " }),
+});
 export const StoreModal = () => {
-    const storeModal = useStoreModal(); 
-
-    return (
-        <Modal
-            title="Create a new store"
-            description="Add a new store to manage the products and categories"
-            isOpen={storeModal.isOpen}
-            onClose={storeModal.onClose} 
-        >
-        form to create the store
-        </Modal>
-    );
-}
+  const storeModal = useStoreModal();
+  const [isLoading, setLoading] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  return (
+    <Modal
+      title="Create a new store"
+      description="Add a new store to manage the products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className="Space-y-4 py-6 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        placeholder="your stroe name ..."
+                        {...field}
+                      ></Input>
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}/>
+            
+              <div className="pt-6 space-x-6 flex items-center justify-end w-full">
+                <Button
+                  disabled={isLoading}
+                  type="button"
+                  variant={"outline"}
+                  size={"sm"}
+                >
+                  Cancle
+                </Button>
+                <Button disabled={isLoading} type="submit" size={"sm"}>
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  );
+};
